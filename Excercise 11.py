9f7ee87f3d9b25187f392d882b454bfc95889335
chrismeim <144551793+chrismeim@users.noreply.github.com>
2023-09-26 15:18:41 +0000

    Change Ex 10-11

--- Excercise 11.py
+++ Excercise 11.py
@@ -5,10 +5,10 @@ n = int(input("Give me the second number "))
 
 my_list = []
 
-def matrix(a,b):
-    for i in range(a):
+def matrix(first_number,second_number):
+    for i in range(first_number):
         list2 = []                   #this is a list that I created in order to store temporarily the numbers that are going to be generated in the second loop
-        for j in range(b):
+        for j in range(second_number):
             list2.append(i*j)     
         my_list.append(list2)        #through this apend I store the temporary list into my main list, which is basicaly the matrix   
     return my_list
