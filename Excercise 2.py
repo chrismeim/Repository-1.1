98c6894573af3554634275d99d864adf1e35dc31
chrismeim <144551793+chrismeim@users.noreply.github.com>
2023-09-26 09:48:06 +0000

    Changed ex2

--- Excercise 2.py
+++ Excercise 2.py
@@ -1,9 +1,10 @@
 type = (input("Are you converting celcius to fahrenheit? "))
-degrees = int(input("Give me the temperature"))
+degrees = int(input("Give me the temperature "))
 
 def converter(x, z):
     if x == "yes":
         f = (z*9/5) + 32
+        print(f"{z} degrees Celcius = {f} fahrenheit")
         return f
     else: 
         c = 5/9*(z-32)
