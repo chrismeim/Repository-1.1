a27b03c148bbde689b3312aee73bb13ded18b41f
chrismeim <144551793+chrismeim@users.noreply.github.com>
2023-10-02 21:23:45 +0000

    Excercise 18

---
+++ Excercise 18.py
@@ -0,0 +1,11 @@
+result = ""
+
+for i in range(0,7):    #number of rows
+    for j in range(0,7):  #number of columns
+        if ((i == 1 or i == 6) and ( j ==1 or j == 2 or j==3 or j ==4 or j == 5)) or (i == 2 or i == 3 or i == 4 or i ==5) and ( j == 1 or j == 6 ):
+            result = result + "*" 
+        else:
+            result = result + " "
+    result = result + "\n"
+
+    print(result)
