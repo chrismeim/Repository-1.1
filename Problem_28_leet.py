71e64c119cc0f17178634124333bb07e1e4ed6f6
chrismeim <144551793+chrismeim@users.noreply.github.com>
2023-10-18 15:09:45 +0000

    minor changes

--- Problem_28_leet.py
+++ Problem_28_leet.py
@@ -6,14 +6,14 @@ list(needle)
 
 index_value = []
 i = 0
-while i < len(haystack):
+while i < len(haystack):     
     dummy = 0
-    if haystack[i] == needle[0]:
-        for k in range(i,len(needle)+i):
+    if haystack[i] == needle[0]:                #if we find that a value from the haystack is equal to a value from the needle we start a loop search
+        for k in range(i,len(needle)+i):        # example, if i = 5, then the loop will search from 5 to 5+2=7 at the haystack and on the needle it will search 5-5=0 to 7-5=2, which is the length of the needle
             if haystack[k] == needle[k-i]:
                 dummy +=1
         if dummy == len(needle):
-            index_value.append(i)
+            index_value.append(i)               #here I append the index value "i" where equality started
     i += 1
 
 print(index_value)
